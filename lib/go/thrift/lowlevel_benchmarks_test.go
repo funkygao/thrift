0a1065f256d4c73bd034236c0c5d39b23e2568a7
funkygao <funky.gao@gmail.com>
2015-02-28 16:43:07 +0800

    show alloc and io throughput in benchmark tests

--- lib/go/thrift/lowlevel_benchmarks_test.go
+++ lib/go/thrift/lowlevel_benchmarks_test.go
@@ -27,132 +27,180 @@ import (
 var binaryProtoF = NewTBinaryProtocolFactoryDefault()
 var compactProtoF = NewTCompactProtocolFactory()
 
-var buf = bytes.NewBuffer(make([]byte, 0, 1024))
+const bufsize = 1024
+
+var buf = bytes.NewBuffer(make([]byte, 0, bufsize))
 
 var tfv = []TTransportFactory{
-	NewTMemoryBufferTransportFactory(1024),
+	NewTMemoryBufferTransportFactory(bufsize),
 	NewStreamTransportFactory(buf, buf, true),
-	NewTFramedTransportFactory(NewTMemoryBufferTransportFactory(1024)),
+	NewTFramedTransportFactory(NewTMemoryBufferTransportFactory(bufsize)),
+}
+
+var listBytesOverhead = 1 + 4 // thetype(byte) * thelen(int32)
+
+func readWriteXXBytes(bytes int) int64 {
+	return int64(listBytesOverhead+bytes) * 2 // rw
 }
 
 func BenchmarkBinaryBool_0(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[0].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteBool(b, p, trans)
 	}
-}
-
-func BenchmarkBinaryByte_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteByte(b, p, trans)
-	}
-}
-
-func BenchmarkBinaryI16_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteI16(b, p, trans)
-	}
-}
-
-func BenchmarkBinaryI32_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteI32(b, p, trans)
-	}
-}
-func BenchmarkBinaryI64_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteI64(b, p, trans)
-	}
-}
-func BenchmarkBinaryDouble_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteDouble(b, p, trans)
-	}
-}
-func BenchmarkBinaryString_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteString(b, p, trans)
-	}
-}
-func BenchmarkBinaryBinary_0(b *testing.B) {
-	trans := tfv[0].GetTransport(nil)
-	p := binaryProtoF.GetProtocol(trans)
-	for i := 0; i < b.N; i++ {
-		ReadWriteBinary(b, p, trans)
-	}
+	b.SetBytes(readWriteXXBytes(len(BOOL_VALUES)))
 }
 
 func BenchmarkBinaryBool_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteBool(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(BOOL_VALUES)))
+}
+
+func BenchmarkBinaryByte_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteByte(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(BYTE_VALUES)))
 }
 
 func BenchmarkBinaryByte_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteByte(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(BYTE_VALUES)))
+}
+
+func BenchmarkBinaryI16_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteI16(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(INT16_VALUES) * 2))
 }
 
 func BenchmarkBinaryI16_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteI16(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(INT16_VALUES) * 2))
+}
+
+func BenchmarkBinaryI32_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteI32(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(INT32_VALUES) * 4))
 }
 
 func BenchmarkBinaryI32_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteI32(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(INT32_VALUES) * 4))
 }
+
+func BenchmarkBinaryI64_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteI64(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(INT64_VALUES) * 8))
+}
+
 func BenchmarkBinaryI64_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteI64(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(INT64_VALUES) * 8))
 }
+
+func BenchmarkBinaryDouble_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteDouble(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(DOUBLE_VALUES) * 16))
+}
+
 func BenchmarkBinaryDouble_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteDouble(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(DOUBLE_VALUES) * 16))
 }
+
+func BenchmarkBinaryString_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteString(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(STRING_VALUES)))
+}
+
 func BenchmarkBinaryString_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteString(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(STRING_VALUES)))
 }
+
+func BenchmarkBinaryBinary_0(b *testing.B) {
+	b.ReportAllocs()
+	trans := tfv[0].GetTransport(nil)
+	p := binaryProtoF.GetProtocol(trans)
+	for i := 0; i < b.N; i++ {
+		ReadWriteBinary(b, p, trans)
+	}
+	b.SetBytes(readWriteXXBytes(len(protocol_bdata)))
+}
+
 func BenchmarkBinaryBinary_1(b *testing.B) {
+	b.ReportAllocs()
 	trans := tfv[1].GetTransport(nil)
 	p := binaryProtoF.GetProtocol(trans)
 	for i := 0; i < b.N; i++ {
 		ReadWriteBinary(b, p, trans)
 	}
+	b.SetBytes(readWriteXXBytes(len(protocol_bdata)))
 }
 
 func BenchmarkBinaryBool_2(b *testing.B) {
